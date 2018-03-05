471a5f456c9df69e51b7a6dd19d907a5c3c7527b
vinceliuice <vinceliuice@hotmail.com>
2018-03-05 15:30:41 +0800

    Update

--- src/xfwm4/render-assets.sh
+++ src/xfwm4/render-assets.sh
@@ -3,23 +3,30 @@
 INKSCAPE="/usr/bin/inkscape"
 OPTIPNG="/usr/bin/optipng"
 
-SRC_FILE="assets.svg"
-ASSETS_DIR="assets"
-
-
 INDEX="assets.txt"
 
-for i in `cat $INDEX`
-do 
-if [ -f $ASSETS_DIR/$i.png ]; then
-    echo $ASSETS_DIR/$i.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i.png
-    $INKSCAPE --export-id=$i \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
-fi
+for variant in '' '-dark'; do
+  for color in '-sea' '-aliz' '-azul'; do
+
+    ASSETS_DIR="assets${variant}${color}"
+    SRC_FILE="assets${variant}${color}.svg"
+
+    install -d $ASSETS_DIR
+
+    for i in `cat $INDEX`
+    do
+    if [ -f $ASSETS_DIR/$i.png ]; then
+        echo $ASSETS_DIR/$i.png exists.
+    else
+        echo
+        echo Rendering $ASSETS_DIR/$i.png
+        $INKSCAPE --export-id=$i \
+                  --export-id-only \
+                  --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
+        && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
+    fi
+    done
+  done
 done
+
 exit 0
