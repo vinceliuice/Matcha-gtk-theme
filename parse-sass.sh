aa54960d930f0bf7672c01d454e0284c66049a8c
vinceliuice <vinceliuice@hotmail.com>
2019-06-05 18:04:56 +0800

    Add light version

--- parse-sass.sh
+++ parse-sass.sh
@@ -7,9 +7,14 @@ fi
 
 SASSC_OPT="-M -t expanded"
 
-_COLOR_VARIANTS=('' '-dark')
+_COLOR_VARIANTS=('' '-light' '-dark')
 if [ ! -z "${COLOR_VARIANTS:-}" ]; then
   IFS=', ' read -r -a _COLOR_VARIANTS <<< "${COLOR_VARIANTS:-}"
+fi
+
+_ECOLOR_VARIANTS=('' '-dark')
+if [ ! -z "${ECOLOR_VARIANTS:-}" ]; then
+  IFS=', ' read -r -a _ECOLOR_VARIANTS <<< "${ECOLOR_VARIANTS:-}"
 fi
 
 _THEME_VARIANTS=('-sea' '-aliz' '-azul')
@@ -24,14 +29,14 @@ for color in "${_COLOR_VARIANTS[@]}"; do
   done
 done
 
-for color in "${_COLOR_VARIANTS[@]}"; do
+for color in "${_ECOLOR_VARIANTS[@]}"; do
   for theme in "${_THEME_VARIANTS[@]}"; do
   sassc $SASSC_OPT src/gnome-shell/gnome-shell${color}${theme}.{scss,css}
   echo "==> Generating the gnome-shell${color}${theme}.css..."
   done
 done
 
-for color in "${_COLOR_VARIANTS[@]}"; do
+for color in "${_ECOLOR_VARIANTS[@]}"; do
   for theme in "${_THEME_VARIANTS[@]}"; do
   sassc $SASSC_OPT src/cinnamon/cinnamon${color}${theme}.{scss,css}
   echo "==> Generating the cinnamon${color}${theme}.css..."
