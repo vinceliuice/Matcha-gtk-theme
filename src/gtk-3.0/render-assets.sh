edacc220e9bacafb578dbdc7aed81c1de5a2de0f
vinceliuice <vinceliuice@hotmail.com>
2021-01-01 19:00:31 +0800

    update

--- src/gtk-3.0/render-assets.sh
+++ src/gtk-3.0/render-assets.sh
@@ -6,34 +6,34 @@ OPTIPNG="/usr/bin/optipng"
 INDEX="assets.txt"
 
 for color in '-aliz' '-azul' '-sea'; do
-
   ASSETS_DIR="assets${color}"
   SRC_FILE="assets${color}.svg"
 
-for i in `cat $INDEX`
-do 
-if [ -f $ASSETS_DIR/$i.png ]; then
-    echo $ASSETS_DIR/$i.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i.png
-    $INKSCAPE --export-id=$i \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png 
-fi
-if [ -f $ASSETS_DIR/$i@2.png ]; then
-    echo $ASSETS_DIR/$i@2.png exists.
-else
-    echo
-    echo Rendering $ASSETS_DIR/$i@2.png
-    $INKSCAPE --export-id=$i \
-              --export-dpi=180 \
-              --export-id-only \
-              --export-png=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null \
-    && $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png 
-fi
+ [ -d $ASSETS_DIR ] && rm -rf $ASSETS_DIR && mkdir -p $ASSETS_DIR
 
+  for i in `cat $INDEX`; do
+    if [ -f $ASSETS_DIR/$i.png ]; then
+      echo $ASSETS_DIR/$i.png exists.
+    else
+      echo
+      echo Rendering $ASSETS_DIR/$i.png
+      $INKSCAPE --export-id=$i \
+                --export-id-only \
+                --export-filename=$ASSETS_DIR/$i.png $SRC_FILE >/dev/null
+      $OPTIPNG -o7 --quiet $ASSETS_DIR/$i.png
+    fi
+    if [ -f $ASSETS_DIR/$i@2.png ]; then
+      echo $ASSETS_DIR/$i@2.png exists.
+    else
+      echo
+      echo Rendering $ASSETS_DIR/$i@2.png
+      $INKSCAPE --export-id=$i \
+                --export-dpi=192 \
+                --export-id-only \
+                --export-filename=$ASSETS_DIR/$i@2.png $SRC_FILE >/dev/null
+      $OPTIPNG -o7 --quiet $ASSETS_DIR/$i@2.png
+    fi
+  done
 done
-done
+
 exit 0
