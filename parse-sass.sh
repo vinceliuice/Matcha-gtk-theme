4af072360bc94c06402ed0c33eb59ff358c73d0e
vinceliuice <vinceliuice@hotmail.com>
2023-04-03 17:12:37 +0800

    Fixed libadwaita issues

--- parse-sass.sh
+++ parse-sass.sh
@@ -39,6 +39,8 @@ for color in "${_ECOLOR_VARIANTS[@]}"; do
   echo "==> Generating the 40.0 gnome-shell${color}${theme}.css..."
   sassc $SASSC_OPT src/gnome-shell/42.0/gnome-shell${color}${theme}.{scss,css}
   echo "==> Generating the 42.0 gnome-shell${color}${theme}.css..."
+  sassc $SASSC_OPT src/gnome-shell/44.0/gnome-shell${color}${theme}.{scss,css}
+  echo "==> Generating the 44.0 gnome-shell${color}${theme}.css..."
   done
 done
 
