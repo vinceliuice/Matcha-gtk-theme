923789ae6a8a4d239aa5935ad4c3b56219e74568
Vince <vinceliuice@hotmail.com>
2025-08-02 00:57:34 +0800

    Update update-matcha-online.sh

--- update-matcha-online.sh
+++ update-matcha-online.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 notify-send "Matcha Theme" "Getting the latest version of the Matcha theme..." -i system-software-update
 cd /tmp/; rm -Rf /tmp/matcha.zip 2>/dev/null
-rm -Rf /tmp/matcha-master/ 2>/dev/null
-wget https://github.com/vinceliuice/matcha/archive/master.zip -O matcha.zip
-unzip matcha.zip; cd matcha-master
+rm -Rf /tmp/Matcha-gtk-theme-master/ 2>/dev/null
+wget https://github.com/vinceliuice/Matcha-gtk-theme/archive/master.zip -O matcha.zip
+unzip matcha.zip; cd Matcha-gtk-theme-master
 ./install.sh
 notify-send "All done!" "Enjoy the latest version of Matcha!" -i face-smile
